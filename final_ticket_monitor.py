0bd7286a306bc88af795808c7cc9b9c46b48e879
Rob <52356169+R0B3R799@users.noreply.github.com>
2022-07-14 10:08:27 +0100

    Add files via upload

--- final_ticket_monitor.py
+++ final_ticket_monitor.py
@@ -20,8 +20,14 @@ class NFTWebhook():
     def personal_webhook(asset, ether_amount, wallet_address):
         # webhook = DiscordWebhook(
         #     url='https://discord.com/api/webhooks/996771713868111892/23PdREOHd-g8cm9NJp_qO2604XlT7AIHACo08_lVNJmPrMUWSI5-G7GTvQ_mQWTe3Fdc') ## main webhook
-        webhook = DiscordWebhook(
-            url='https://discord.com/api/webhooks/996795962188107867/M5Io31Au76jqeXz4dhlucCp3rWudw5pvTYQ7hMXEbaxphJ_K0cQzfCC5a8zpbJ2e6vLO')  #big baller
+        # webhook = DiscordWebhook(
+        #     url='https://discord.com/api/webhooks/996795962188107867/M5Io31Au76jqeXz4dhlucCp3rWudw5pvTYQ7hMXEbaxphJ_K0cQzfCC5a8zpbJ2e6vLO')  #big baller
+        if wallet_address == '0x504370ce2abef4ebea8c0aac33abdb30160e31e4':
+            webhook = DiscordWebhook(
+                url='https://discord.com/api/webhooks/997066333613195305/NEArXdxSQRIkCYBQUaPNjndzAOXIibU3mtmnw5dOo6ryxiiYSAGDoB22wn849mWkbK_d')  #big baller 2
+        else:
+            webhook = DiscordWebhook(
+                url='https://discord.com/api/webhooks/996795962188107867/M5Io31Au76jqeXz4dhlucCp3rWudw5pvTYQ7hMXEbaxphJ_K0cQzfCC5a8zpbJ2e6vLO')  #big baller   
         embed = DiscordEmbed(title=f"New Coin "+asset.name,color=2021216) 
         embed.set_thumbnail(url="https://etherscan.io/images/main/empty-token.png")
         embed.add_embed_field(name="ETH amount Of Buy | Number Of "+asset.symbol, value=str(ether_amount)+'    |    '+str(asset.amount_of_tokens), inline=False)
@@ -87,7 +93,7 @@ class NFShitCoins():
 
 #wallets = ['0x079386dcf2f4b571b804112af97ff9d5ac2c538e','0xb0d6b433c850aa0afabed1532993866d7f7e507e','0x79979be80f16a7600297e0ce23d08d1a5e174aee','0x8722c7d5aa13e400016576eb634c1a1407805415','0x5cde4882124dc1d3ed475d3c162e7e08d7e510ba','0x3467eba885767d3ac9e754380036ab04d275942a']
 
-wallets = ['0xbcb05a3f85d34f0194c70d5914d5c4e28f11cc02']
+wallets = ['0x504370ce2abef4ebea8c0aac33abdb30160e31e4','0xbcb05a3f85d34f0194c70d5914d5c4e28f11cc02']
 
 i = 0
 for i in range(len(wallets)):
